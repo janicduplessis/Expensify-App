13de5ad3a8f4ee376ac7df2af0cf4f2a7736c429
Janic Duplessis <janicduplessis@gmail.com>
2024-10-23 17:39:11 -0400

    Open reports at first unread action

--- src/components/InvertedFlatList/BaseInvertedFlatList/index.tsx
+++ src/components/InvertedFlatList/BaseInvertedFlatList/index.tsx
@@ -111,12 +111,31 @@ function BaseInvertedFlatList<T>(props: BaseInvertedFlatListProps<T>, ref: Forwa
             });
         };
 
+        const scrollToIndexFn: RNFlatList['scrollToIndex'] = (params) => {
+            const actualIndex = params.index + dataIndexDifference;
+            try {
+                listRef.current?.scrollToIndex({...params, index: actualIndex});
+            } catch (ex) {
+                // It is possible that scrolling fails since the item we are trying to scroll to
+                // has not been rendered yet. In this case, we call the onScrollToIndexFailed.
+                props.onScrollToIndexFailed?.({
+                    index: actualIndex,
+                    // These metrics are not implemented.
+                    averageItemLength: 0,
+                    highestMeasuredFrameIndex: 0,
+                });
+            }
+        };
+
         return new Proxy(
             {},
             {
                 get: (_target, prop) => {
                     if (prop === 'scrollToOffset') {
                         return scrollToOffsetFn;
+                    }
+                    if (prop === 'scrollToIndex') {
+                        return scrollToIndexFn;
                     }
                     return listRef.current?.[prop as keyof RNFlatList];
                 },
